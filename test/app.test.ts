ee799f1b73dc817035f92f1f0f904ed292013f30
brunocoronado49 <francorangelcoronado@gmail.com>
2026-01-08 10:53:50 -0600

    App testing (finish)

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,5 +1,32 @@
-describe('App', () => {
-  test('Should be true', () => {
-    expect(true).toBe(true);
+import { ServerApp } from '../src/presentation/server-app';
+
+describe('Test App.ts', () => {
+  test('Should call Server.run with values', async () => {
+    const serverRunMock = jest.fn();
+    ServerApp.run = serverRunMock;
+
+    process.argv = [
+      'node',
+      'app.ts',
+      '-b',
+      '10',
+      '-l',
+      '5',
+      '-s',
+      '-n',
+      'test-file',
+      '-d',
+      'test-destination',
+    ];
+
+    await import('../src/app');
+
+    expect(serverRunMock).toHaveBeenCalledWith({
+      base: 10,
+      limit: 5,
+      showTable: true,
+      fileName: 'test-file',
+      fileDestination: 'test-destination',
+    });
   });
 });
